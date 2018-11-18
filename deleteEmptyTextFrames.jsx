163a05c1fefbc128030a1e6e779480fc4eaee2a2
Reiko_Arai <13619595+araiprepress@users.noreply.github.com>
2018-11-18 18:45:42 +0900

    feat:Added 2 script files:
    deleteEmptyTextFrames.jsx and deleteUnusedPasses.jsx

---
+++ deleteEmptyTextFrames.jsx
@@ -0,0 +1,50 @@
+﻿//target application is Illustrator CS6-CC; NOT for InDesign
+//Delete all the empty text frames.
+//For locked layers, unlock and lock again after deleting empty text frames on the layer.
+
+layTF=[];
+layObj = app.activeDocument.layers;
+layLength=layObj.length;
+
+myTObj=app.activeDocument.textFrames;
+len=myTObj.length;
+pat = new RegExp("^[\u0020\u3000\u00a0\t\s]*$","g");
+
+if (myTObj.length==0){
+	alert("no text items available");
+	exit();
+}
+
+unlockLayers();
+
+n=0;
+for(i=len-1; i>=0; i--){
+	myContents=myTObj[i].contents+"";
+
+	if (myContents.match(pat)){
+		myTObj[i].remove();	
+		n++;
+	}
+}
+layerRestore();
+
+alert(n+" empty text items are removed.");
+
+
+function unlockLayers(){
+
+	for (j=0; j<layObj.length; j++)
+	{ 
+	       layTF[j]=layObj[j].locked;
+	       if (layObj[ j ].locked == true) layObj[ j ].locked = false;
+	}
+}
+
+
+function layerRestore(){
+
+	for(k=0; k<layTF.length; k++){
+		if(layTF[k]==true)layObj[k].locked=true;
+	}
+}
+
---
+++ deleteUnusedPasses.jsx
@@ -0,0 +1,50 @@
+﻿//target application is Illustrator CS6-CC; NOT for InDesign
+//Delete all the path items like the followings:
+// - paths without fill or stroke, 
+//        and 
+// - not clipping path or not wrapped.
+
+//For compoundPathItems, delete if ALL the pathitems contained are transparent.
+
+
+if(app.documents.length==1){
+	checkCompoundPathItems();
+	deleteUnusedPathItems();
+} else alert("Open 1 document");
+
+
+function deleteUnusedPathItems(){
+	pLen = app.activeDocument.pathItems.length;
+	for (i=pLen-1; i>=0; i--){
+		var pObj = app.activeDocument.pathItems[i];
+		if (!pObj.filled && !pObj.stroked){ 
+			if(!pObj.wrapped && !pObj.clipping){
+				pObj.remove();
+			}
+		}
+	}
+}
+
+function checkCompoundPathItems(){
+	cLen = app.activeDocument.compoundPathItems.length;
+	if(cLen > 0){
+		v = 0;
+		for(i=cLen-1; i>=0; i--){
+			compObj = app.activeDocument.compoundPathItems[i];
+			cPLen = compObj.pathItems.length;
+				for(j=0; j<cPLen; j++){
+					if (!compObj.pathItems[j].filled && !compObj.pathItems[j].stroked){
+						v = v + 1;
+					}
+					// alert(v);
+				}
+			if(v > 0){
+				compObj.remove();
+			}
+		}
+	}
+
+}
+
+// alert(app.activeDocument.pathItems.length)
+alert(app.activeDocument.compoundPathItems[0].pathItems[0].filled)
