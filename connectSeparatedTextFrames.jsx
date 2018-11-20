a9270c7161bc3e11bb5413627824b32176cde78a
Reiko_Arai <13619595+araiprepress@users.noreply.github.com>
2018-11-20 12:54:24 +0900

    feat: Newly Upload connectSeparatedTextFrames.jsx
    Newly Upload connectSeparatedTextFrames.jsx

---
+++ connectSeparatedTextFrames.jsx
@@ -0,0 +1,29 @@
+﻿// Concatenate textframes scattered apart.
+// Connecting order is simply from left to right.
+// *asserted only vertical textframes;
+// Usage: Select textframes to concatenate.
+
+var selObj = app.activeDocument.selection;
+var sLen = selObj.length;
+var dc = {};
+var tXyArray = [];
+var str = "";
+for(i=0; i<sLen; i++){
+	if (selObj[i].typename =="TextFrame"){
+		dc['top'] = selObj[i].top;
+		dc['left'] = selObj[i].left;
+		dc['cont'] = selObj[i].contents;
+		tXyArray.push(dc);
+	}
+	dc = {};
+}
+
+tXyArray = tXyArray.sort(function(a, b){
+	return a.left - b.left;
+});
+
+for(j=0; j<tXyArray.length; j++){
+    str+= tXyArray[j].cont;
+}
+txtObj = app.activeDocument.textFrames.add();
+txtObj.contents = str;
