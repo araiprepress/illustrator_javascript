0c9237845984926b41e4730129d443ff96def985
Reiko_Arai <13619595+araiprepress@users.noreply.github.com>
2018-11-30 23:36:07 +0900

    feat:Upload clippingPathFillandStroke.jsx

---
+++ clippingPathFillandStroke.jsx
@@ -0,0 +1,29 @@
+﻿//target application is Illustrator (CS6-)CC; NOT for InDesign
+//If a clipping path has stroke or fill, 
+//copy the path, delete the stroke or fill value, and paste.
+
+
+if(app.documents.length==1){
+	separateStrokedClippingPath()
+} else alert("Open 1 document");
+
+
+function separateStrokedClippingPath(){
+	pLen = app.activeDocument.pathItems.length;
+	for (i=0; i<pLen; i++){
+		var pObj = app.activeDocument.pathItems[i];
+		if (pObj.clipping){
+			if(pObj.stroked || pObj.filled){ 
+				_pObj = pObj.duplicate();
+				_pObj.clipping = false;
+
+				pObj.stroked = false;
+				pObj.filled = false;
+				pObj.selected = false;
+				
+		}
+	}
+}
+}
+
+
