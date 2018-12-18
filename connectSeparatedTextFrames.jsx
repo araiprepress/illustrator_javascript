0b7e822472537607d0cd3ad98729376b042288ad
Reiko Arai <13619595+araiprepress@users.noreply.github.com>
2018-12-18 17:26:42 +0900

    fix: Correct new txtframe's location
    Corrected new txtFrames' location from (0,0) to current location on a new layer

--- connectSeparatedTextFrames.jsx
+++ connectSeparatedTextFrames.jsx
@@ -1,13 +1,16 @@
-// Concatenate textframes scattered apart.
+﻿// Concatenate textframes scattered apart.
 // Connecting order is simply from left to right.
 // *asserted only horizontal textframes;
 // Usage: Select textframes to concatenate.
+// A connected textframe apperas on a new layer at the x-y position of the first left item.
 
 var selObj = app.activeDocument.selection;
 var sLen = selObj.length;
 var dc = {};
 var tXyArray = [];
 var str = "";
+var layObj = activeDocument.layers.add();
+
 for(i=0; i<sLen; i++){
 	if (selObj[i].typename =="TextFrame"){
 		dc['top'] = selObj[i].top;
@@ -27,3 +30,6 @@ for(j=0; j<tXyArray.length; j++){
 }
 txtObj = app.activeDocument.textFrames.add();
 txtObj.contents = str;
+txtObj.position = Array(tXyArray[0].left, tXyArray[0].top)
+txtObj.moveToBeginning(layObj);
+
